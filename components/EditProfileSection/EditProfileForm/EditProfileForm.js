88f15c45331b4352e20f9df9ed321c48bd094f4f
soykeepgoing <soykeepgoing@gmail.com>
2025-12-13 16:50:09 +0900

    feat: 프로필 수정 내 단방향 정보 전달로 컴포넌트 분리

--- components/EditProfileSection/EditProfileForm/EditProfileForm.js
+++ components/EditProfileSection/EditProfileForm/EditProfileForm.js
@@ -2,9 +2,6 @@ import { h } from "../../../core/vdom/h.js";
 import { useState } from "../../../core/hooks/useState.js";
 import { useInputField } from "../../../core/hooks/useInputField.js";
 
-import { validateEmail } from "../../../utils/validation/validateEmail.js";
-import { validatePw } from "../../../utils/validation/validatePw.js";
-import { validatePwCheck } from "../../../utils/validation/validatePwCheck.js";
 import { validateNickname } from "../../../utils/validation/validateNickname.js";
 
 import { Button } from "../../Button/Button.js";
@@ -15,33 +12,26 @@ import { ProfileImgField } from "../../ProfileImgField/ProfileImgField.js";
 import { handleImageChanged } from "../../../handlers/handleImageChanged.js";
 import { handleSignUp } from "../../../handlers/users/handleSignup.js";
 
-export function EditProfileForm(){
-
-    const nickname = useInputField("", validateNickname);
-
-    const [imgPreviewUrl, setImgPreviewUrl] = useState("../../../images/default_user_profile.png");
-
-    let profileImageUrl = null;
-    let profileImageFile = null;
-
-    return h("form",
+export function EditProfileForm({
+    nickname, 
+    imgPreviewUrl, 
+    onImgChange, 
+    onSubmit
+}){
+    return h(
+        "form",
         { className: "edit-profile-form"}, 
         ProfileImgField({
             id: "signup-img-input", 
             label: "Profile Image",
             src: imgPreviewUrl,
-            onChange: async (e) => {
-                const {imageUrl, file} = await handleImageChanged(e);
-                profileImageUrl = imageUrl;
-                profileImageFile = file;
-                setImgPreviewUrl(imageUrl);
-            }
+            onChange: onImgChange
         }),
         EmailInfo(), 
         FormField({
             label: "Nickname*",
             type: "nickname",
-            id: "editProfile-nickname-input",
+            id: "edit-profile-nickname-input",
             placeholder: "닉네임을 입력하세요.",
             value: nickname.value,
             helperText: nickname.helperText,
@@ -52,14 +42,8 @@ export function EditProfileForm(){
             className: "button",
             id: "editProfile_btn",
             text: "Change Profile", 
-            disabled: ! nickname.isValid,
-            onClick: async (e) => {
-                e.preventDefault();
-                const res = await handleEditProfile({
-                    nickname: nickname.value,
-                    profileImage: profileImageFile
-                });
-            }
+            disabled: !nickname.isValid,
+            onClick: onSubmit
         })
     )
 }
--- components/EditProfileSection/EditProfileSection.js
+++ components/EditProfileSection/EditProfileSection.js
@@ -1,10 +1,49 @@
 import { h } from "../../core/vdom/h.js";
 
+import { EditProfileForm } from "./EditProfileForm/EditProfileForm.js";
+import { LinkTo } from "../LinkTo/LinkTo.js";
+
+import { useState } from "../../core/hooks/useState.js";
+import { useInputField } from "../../core/hooks/useInputField.js";
+
+import { validateNickname } from "../../utils/validation/validateNickname.js";
+import { handleImageChanged } from "../../handlers/handleImageChanged.js";
+
 export function EditProfileSection(){
-    // const [] = useState(); 
-    // const [] = useState(); 
 
+    const nickname = useInputField("", validateNickname);
+    const [imgPreviewUrl, setImgPreviewUrl] = useState(
+        "../../../images/default_user_profile.png");
+    const [profileImgFile, setProfileImgFile] = useState(null);
 
+    const handleProfileImgChange = async(e) => {
+        const {imgUrl, file} = await handleImageChanged(e);
+        setImgPreviewUrl(imgUrl);
+        setProfileImgFile(file);
+    };
 
-    return h("h2", {}, "회원 정보 수정");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await handleEditProfile({
+            nickname: nickname.value, 
+            profileImgFile: profileImgFile
+        });
+    };
+
+    return h(
+        "section", 
+        {}, 
+        h("h2", {}, "회원 정보 수정"),
+        EditProfileForm({
+            nickname, 
+            imgPreviewUrl, 
+            onImgChange: handleProfileImgChange, 
+            onSubmit: handleSubmit
+        }), 
+        LinkTo({
+            text: "Sign Out", 
+            onClick: () => console.log("Sign Out")
+        })
+    );
 }
